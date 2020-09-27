6319a9b4d82df45f9d5ad9ca92f7216e3efdff70
thomas maestas <thomasm1.maestas@gmail.com>
2020-09-26 23:22:13 -0400

    friends of groot: update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   // baseUrl: "localhost:8082/",
   baseUrl: "http://54.174.82.153:8099",
-  marvelKey: "dce73dc128d232d3a6c22decdf3b5f272bbc5178",
+  marvelKey: '5fbd9e22b0c348faa25fd3d07bee8248',
+  marvelHash: 'dce73dc128d232d3a6c22decdf3b5f272bbc5178',
   apiKeyNYT: "06voWGzUHt0AJNvF2CeIqGezsRBQTZd5",
   apiUrlNYT: "https://api.nytimes.com/svc"
 };
